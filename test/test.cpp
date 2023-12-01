8170169ba54e6cf632add327eb0e10562560ac6e
0a777h <0a777h@gmail.com>
2023-12-01 21:02:39 +0900

    update

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+//#define USE_VM_MACRO
+#ifdef USE_VM_MACRO
+#ifdef _WIN64
+#pragma comment(lib, "vxlib64.lib")
+#else
+#pragma comment(lib, "vxlib32.lib")
+#endif
+
 void __stdcall VxVirtualizationBegin();
 void __stdcall VxVirtualizationEnd();
 
@@ -31,29 +39,39 @@ void __stdcall VxCodeFlatteningLv5End();
 
 //
 
-#define VL_OBFUSCATION_BEGIN         VxObfuscationBegin()
-#define VL_OBFUSCATION_END           VxObfuscationEnd()
+#define VL_OBFUSCATION_BEGIN               VxObfuscationBegin()
+#define VL_OBFUSCATION_END                 VxObfuscationEnd()
 
-#define VL_CODE_FLATTENING_BEGIN     VxCodeFlatteningBegin()
-#define VL_CODE_FLATTENING_END       VxCodeFlatteningEnd()
+#define VL_CODE_FLATTENING_BEGIN           VxCodeFlatteningBegin()
+#define VL_CODE_FLATTENING_END             VxCodeFlatteningEnd()
 
 #define VL_CODE_FLATTENING_TYPE2_BEGIN     VxCodeFlatteningType2Begin()
 #define VL_CODE_FLATTENING_TYPE2_END       VxCodeFlatteningType2End()
 
-#define VL_VIRTUALIZATION_BEGIN      VxVirtualizationBegin()
-#define VL_VIRTUALIZATION_END        VxVirtualizationEnd()
+#define VL_VIRTUALIZATION_BEGIN            VxVirtualizationBegin()
+#define VL_VIRTUALIZATION_END              VxVirtualizationEnd()
 
 //
 
 #define VL_CODE_FLATTENING_LV_BEGIN(lv)     VxCodeFlatteningLv##lv##Begin()
 #define VL_CODE_FLATTENING_LV_END(lv)       VxCodeFlatteningLv##lv##End()
+#else
+#define VL_OBFUSCATION_BEGIN            
+#define VL_OBFUSCATION_END              
+
+#define VL_CODE_FLATTENING_BEGIN        
+#define VL_CODE_FLATTENING_END          
+
+#define VL_CODE_FLATTENING_TYPE2_BEGIN  
+#define VL_CODE_FLATTENING_TYPE2_END    
+
+#define VL_VIRTUALIZATION_BEGIN         
+#define VL_VIRTUALIZATION_END           
 
 //
 
-#ifdef _WIN64
-#pragma comment(lib, "vxlib64.lib")
-#else
-#pragma comment(lib, "vxlib32.lib")
+#define VL_CODE_FLATTENING_LV_BEGIN(lv) 
+#define VL_CODE_FLATTENING_LV_END(lv)   
 #endif
 
 //
@@ -84,8 +102,6 @@ int main() {
     VL_CODE_FLATTENING_LV_END(3);
 
 #if 0
-    //
-
     VL_CODE_FLATTENING_LV_BEGIN(4);
 
     printf("Lv.4 ! \n");
